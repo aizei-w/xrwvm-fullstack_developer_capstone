43b109ef2e6c2e1ae2dc24efd254fc3d2c40c655
aizei-w <arinlyn1111@gmail.com>
2026-08-29 09:49:08 +0900

    Add Django application routes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,18 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-# from . import views
+from django.urls import path
 
-app_name = 'djangoapp'
+from . import views
+
+app_name = "djangoapp"
+
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("login", views.login_user, name="login"),
+    path("logout", views.logout_user, name="logout"),
+    path("register", views.registration, name="register"),
+    path("get_dealers", views.get_dealerships, name="get_dealers"),
+    path("get_dealers/<str:state>", views.get_dealerships, name="get_dealers_state"),
+    path("dealer/<int:dealer_id>", views.get_dealer_details, name="dealer_details"),
+    path("reviews/dealer/<int:dealer_id>", views.get_dealer_reviews, name="dealer_reviews"),
+    path("add_review", views.add_review, name="add_review"),
+    path("get_cars", views.get_cars, name="get_cars"),
+    path("analyze/<path:text>", views.analyze_review, name="analyze_review"),
+]
