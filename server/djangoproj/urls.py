8b332259595c12adc1d1d885c81532a645702c99
aizei-w <arinlyn1111@gmail.com>
2026-08-29 09:52:55 +0900

    Serve course landing page and React routes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-from django.conf import settings
-from django.http import HttpResponse
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
-
-
-def react_app(request):
-    if not (settings.FRONTEND_BUILD_DIR / "index.html").exists():
-        return HttpResponse("React build not found. Run npm run build.", status=503)
-    return TemplateView.as_view(template_name="index.html")(request)
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -18,6 +9,6 @@ urlpatterns = [
     path("about/", TemplateView.as_view(template_name="About.html")),
     path("contact", TemplateView.as_view(template_name="Contact.html")),
     path("contact/", TemplateView.as_view(template_name="Contact.html")),
-    path("", react_app, name="home"),
-    re_path(r"^(?:login|register|dealers|dealer/\d+|postreview/\d+)/?$", react_app),
+    path("", TemplateView.as_view(template_name="Home.html"), name="home"),
+    re_path(r"^(?:login|register|dealers|dealer/\d+|postreview/\d+)/?$", TemplateView.as_view(template_name="index.html")),
 ]
